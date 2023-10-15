03a23ee055f3eb9497a48aa23c18451a16b605f2
guhrabelo <gustavo.rabelo.teles01@gmail.com>
2023-10-14 21:44:26 -0300

    inserindo testes

---
+++ 05_PrimeirosPassosSpringBoot/rest-spring-boot/src/test/java/br/com/rabelo/unittests/mockito/services/PersonServicesTest.java
@@ -0,0 +1,80 @@
+package br.com.rabelo.unittests.mockito.services;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import br.com.rabelo.model.Person;
+import br.com.rabelo.repository.PersonRepository;
+import br.com.rabelo.services.PersonServices;
+import br.com.rabelo.unittests.mapper.mocks.MockPerson;
+
+@TestInstance(Lifecycle.PER_CLASS)
+@ExtendWith(MockitoExtension.class)
+class PersonServicesTest {
+
+	MockPerson input;
+	
+	@InjectMocks
+	private PersonServices service;
+	
+	@Mock
+	PersonRepository repository;
+	
+	@BeforeEach
+	void setUpMocks() throws Exception {
+		input = new MockPerson();
+		MockitoAnnotations.openMocks(this);
+	}
+
+	@Test
+	void testCreate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testFindAll() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testFindById() {
+		Person person = input.mockEntity(1);
+		person.setId(1L);
+		
+		when(repository.findById(1L)).thenReturn(Optional.of(person));
+		
+		var result = service.findById(1L);
+		assertNotNull(result);
+		assertNotNull(result.getKey());
+		assertNotNull(result.getLinks());
+		System.out.println(result.toString());
+		assertTrue(result.toString().contains("links: [</api/person/v1/1>;rel=\"self\"]"));
+		assertEquals("First Name Test1", result.getFirstName());
+		assertEquals("Last Name Test1", result.getLastName());
+		assertEquals("Female", result.getGender());
+		assertEquals("Addres Test1", result.getAddress());
+	}
+
+	@Test
+	void testUpdate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDelete() {
+		fail("Not yet implemented");
+	}
+
+}
